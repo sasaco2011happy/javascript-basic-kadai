4f091bbeef2f0e5defb6ac219e79c6e46a801e17
sasaco2011happy <sasa2011happy@yahoo.co.jp>
2024-10-27 16:45:26 +0900

    kadai_007 データUP

---
+++ kadai_007/calc.js
@@ -0,0 +1,24 @@
+
+
+ // 変数numに0～4までのランダムな整数を代入する
+ let num = Math.floor(Math.random() * 15);
+ 
+ // 変数numの値を出力する（確認用）
+ console.log(num);
+
+//変数numが3の倍数の場合
+if(num % 3 === 0){
+    console.log('3の倍数です');
+}
+
+else if(num % 5 === 0){
+    console.log('5の倍数です');
+}
+
+else if(num % 3 === 0 && num % 5 === 0){
+    console.log('3と5の倍数です');
+}
+
+else {
+    console.log(num);
+}
